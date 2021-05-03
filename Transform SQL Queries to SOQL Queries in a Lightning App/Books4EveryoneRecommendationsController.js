adfed4439d3507aa98f6403b773881bc33004854
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-05-03 12:09:04 +0200

    Update Books4EveryoneRecommendationsController.js

--- Transform SQL Queries to SOQL Queries in a Lightning App/Books4EveryoneRecommendationsController.js
+++ Transform SQL Queries to SOQL Queries in a Lightning App/Books4EveryoneRecommendationsController.js
@@ -1,5 +1,10 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+  doInit: function(component, event, helper) {
+    var action = component.get("c.getBookRecommendations");
+    action.setCallback(this, function(data) {
+      component.set("v.Recommendations", data.getReturnValue());
+      console.log(data.getReturnValue()); });
+    $A.enqueueAction(action);
+  }
 })
+      
