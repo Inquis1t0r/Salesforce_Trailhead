f839b7e2d634d902b620ecf61c8d29c91b8a143d
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-05-03 12:14:37 +0200

    Update Books4EveryoneMissingAuthorsController.js

--- Transform SQL Queries to SOQL Queries in a Lightning App/Books4EveryoneMissingAuthorsController.js
+++ Transform SQL Queries to SOQL Queries in a Lightning App/Books4EveryoneMissingAuthorsController.js
@@ -1,5 +1,11 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+  doInit: function(component, event, helper) {
+    var action = component.get("c.getBooksWithoutAuthors");
+    action.setCallback(this, function(data) {
+      component.set("v.Books", data.getReturnValue());
+      console.log(data.getReturnValue());
+    });
+    $A.enqueueAction(action);
+  }
 })
+    
