48eb634e959249dd2048efc205f7171988f33475
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-07-04 11:13:40 +0200

    Update DeclaringVariables.js

--- JavaScript Skills for Salesforce Developers/DeclaringVariables.js
+++ JavaScript Skills for Salesforce Developers/DeclaringVariables.js
@@ -6,3 +6,15 @@ const numberOfGears = 12;
 myBike = "Penny Farthing"; // this works
 currentGear = 1; // so does this
 numberOfGears = 1; // error
+
+// call constructor, new object, assign it to bike
+const bike = new Bike();
+//Change internal state by calling a function
+bike.changeGear("front", "Up");
+// add a new member to bike that did not exist before
+bike.type = "Penny Farthing";
+// check for success
+console.log(bike.calculateGearRatio()); // 4.0909...
+console.log(bike.type); // "Penny Farthing"
+// attempt to point bike to new instance of Bike
+bike = new Bike(1,2); // error
