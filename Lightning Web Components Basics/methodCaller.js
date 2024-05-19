50d5fce56ee5c21701dcf65bceec6a93334cc05f
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-05-19 12:50:24 +0200

    Create methodCaller.js

---
+++ Lightning Web Components Basics/methodCaller.js
@@ -0,0 +1,7 @@
+// methodCaller.js
+import { LightningElement } from 'lwc';
+export default class MethodCaller extends LightningElement {
+  handlePlay() {
+    this.template.querySelector('c-video-player').play();
+  }
+}
